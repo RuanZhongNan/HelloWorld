63b7ba015bd46a489745ac552ef7a0a570bdc291
ruan-cat <1219043956@qq.com>
2024-10-09 20:22:44 +0800

    🎉 init:

---
+++ src/layouts/Layouts.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+</script>
+
+<template>
+  <main class="layouts-root">
+    <RouterView></RouterView>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.layouts-root {
+}
+</style>
