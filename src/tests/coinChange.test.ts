d2a4a9823021b6222409362a92e4732782e0d54a
ruan-cat <1219043956@qq.com>
2024-10-09 20:05:45 +0800

    🐞 fix: 修复 coinChange 银币找零；

--- src/tests/coinChange.test.ts
+++ src/tests/coinChange.test.ts
@@ -1,18 +1,26 @@
 import { expect, test } from "vitest";
 
-let COINS = [64, 16, 4, 1];
-function coinChange(rest, idx) {
+const COINS = <const>[64, 16, 4, 1];
+
+function coinChange(rest: number, idx: number): number {
+  console.log(` 现在还剩下 ${rest}，当前的硬币面值是 ${COINS[idx]}`);
+
   if (rest > COINS[idx]) {
-    rest -= COINS[idx];
+    rest = rest - COINS[idx];
     return 1 + coinChange(rest, idx);
-  } else if (rest > 0) {
-    return coinChange(rest, +idx);
+    // FIXME: 如果当前剩余的货币 正好等于币值 则直接返回找1个硬币 退出判断
+  } else if (rest > 0 && rest !== COINS[idx]) {
+    // FIXME: idx应该递增，而不是一直写死
+    // return coinChange(rest, +idx);
+    idx = idx + 1;
+    console.log("now idx", idx);
+    return coinChange(rest, idx);
   }
+
   return 1;
 }
 
 // const coinChangeInput
-
 // function main() {
 //   // let n = parseInt(input.value);
 //   let ans = coinChange(1024 - n, 0);
